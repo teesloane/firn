5c493c40536482ec8f625c465660d8af8fd9647a
teesloane <tylersloane@gmail.com>
2020-03-02 05:50:49 -0500

    Get clj -> rust -> clj working.

--- src/firn/core.clj
+++ src/firn/core.clj
@@ -1,8 +1,15 @@
 (ns firn.core
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [clojure.java.shell :as sh]
+            [cheshire.core :as json])
   (:gen-class))
 
 ;; Files Reading
+;; talk to rust binary!
+(defn parse!
+  [file-str]
+  (sh/sh "./src/parser/target/debug/parser" file-str))
+
 
 (defn get-files
   "Returns a list of files as Java objects.
@@ -22,7 +29,11 @@
   "Takes a list of files and reads them, eventually parsing them."
   [file]
   (println "Reading File..." (.getName file))
-  (slurp file))
+  (let [f (-> file slurp parse!)]
+    (if-not (= (:exit f) 0)
+      (prn "It failed -- handle this.")
+      (json/parse-string (get f :out) true))))
+
 
 
 (defn write-file
@@ -40,12 +51,13 @@
       (-> f
           (read-file)
           (write-file)))))
-
-;; (compile)
+ 
+(compile)
 
 ;; File Writing
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (compile))
+
+
--- src/parser/src/main.rs
+++ src/parser/src/main.rs
@@ -1,8 +1,12 @@
 use orgize::Org;
 use serde_json::{to_string};
+use std::env;
 
 
 fn main() {
-    let org = Org::parse("I 'm *bold*.");
+    let args: Vec<String> = env::args().collect();
+    let input = &args[1];
+    let org = Org::parse(input);
+    // let org = Org::parse("I 'm *bold*.");
     println!("{}", to_string(&org).unwrap());
 }
