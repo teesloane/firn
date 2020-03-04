3a5fc425bf74b31fb83516347eb26c9bb3175839
teesloane <tylersloane@gmail.com>
2020-03-04 10:17:37 -0500

    Mess mess.

--- src/firn/markup.clj
+++ src/firn/markup.clj
@@ -5,6 +5,15 @@
 
 
 ;; Helpers
+
+(defn flatten-one-level [coll]
+  (into [] (mapcat  #(if (sequential? %) % [%]) coll)))
+
+(flatten-one-level [1 [2 3] [4 [5]]])
+
+
+
+;;
 (defn- parse-file-link
   "FIXME move this to regex.
   AND FIXME: add a base_path into config? (with a partial?) "
@@ -41,6 +50,7 @@
 
 
 
+
 (defn to-html
   "RECURSIVE.
   Parses the org-tree tree-seq into html.
@@ -52,9 +62,9 @@
         val        (if value (s/trim-newline value) value)
         inner-html #(if (empty? children)
                       ""
-                      [% (vec (map to-html children))])]
+                      [%  [(flatten-one-level (map to-html children))]])]
    (case type
-      "document"     (map to-html children)
+      "document"     (inner-html :body)
       "headline"     (inner-html :div)
       "title"        (inner-html (title-level->html v))
       "section"      (inner-html :section)
@@ -68,11 +78,62 @@
       "source-block" (src-block->html v)
       "link"         (a->html v)
       "code"         [:code val]
-      "text"         [:span val]
+      "text"         [[:span val]]
       ;; default value.
       [:span (str "<missing type!>" type " val is " value)])))
 
-;; (to-html ex/ex)
+(to-html ex/ex)
+
+
+(into [] (concat))
+
+
+;; => [:body
+;;     ([:section
+;;       ([:span "<missing type!>keyword val is Tenses"]
+;;        [:span "<missing type!>keyword val is 2020-02-28--08-31"]
+;;        [:span "<missing type!>keyword val is research"])]
+;;      [:div
+;;       ([:h1 ([:span "Meta"])]
+;;        [:section ([:span "<missing type!>drawer val is "])])]
+;;      [:div ([:h1 ([:span "Resources"])])]
+;;      [:div
+;;       ([:h1 ([:span "Notes"])]
+;;        [:section
+;;         ([:div
+;;           ([:span "It has taken me learning a "]
+;;            [:a
+;;             {:href "/research_learning_french_2020-02-24--21-20"}
+;;             "new language�"]
+;;            [:span
+;;             " that I have realized I don't know what\r\nmost of the words surrounding describing tenses are in my native language."])]
+;;          [:ul
+;;           ([:li
+;;             ([:div
+;;               ([:span
+;;                 "The simple past in english is used to show a completed action that took place"])])])]
+;;          [:div
+;;           ([:span "at a time in the past. "]
+;;            [:i ([:span "Example"])]
+;;            [:span ": I "]
+;;            [:strong ([:span "lost"])]
+;;            [:span " my wallet on Sunday, so I "]
+;;            [:strong ([:span "bought"])]
+;;            [:span "  a\r\nnew one yesterdray."])]
+;;          [:ul
+;;           ([:li
+;;             ([:div
+;;               ([:span
+;;                 "The present perfect is used with has/have and a past particible. In "]
+;;                [:a {:href "/research_learning_french_2020-02-24--21-20"} "French"]
+;;                [:span ",\r\n  this is similarly done using "]
+;;                [:code "avoir"]
+;;                [:span "."])])])]
+;;          [:div.quote-block ([:div ([:span "fail"])])]
+;;          [:pre "function sum (x, y) {\r\n    return 1 + y\r\n}\r\n"])])])]
+
+
+
 
 ;; (h/html [:div
 ;;           [:span
@@ -87,3 +148,4 @@
            [:head
             [:link {:rel "stylesheet" :href "./assets/main.css"}]]
            [:body {} (h/html (to-html org-tree))]]))
+;; => #'firn.markup/template;; => #'firn.markup/template
