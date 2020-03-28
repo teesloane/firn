8fc7240ed5d9c9064ceaba8ed5bb4be6ae306cc3
teesloane <tylersloane@gmail.com>
2020-03-27 20:52:09 -0400

    Docs: create setup.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((magit-status-mode
-  (magit-todos-exclude-globs "*.org")))
+  (magit-todos-exclude-globs "*.org"))
+ (org-mode
+  (org-link-file-path-type . relative)
+  (time-stamp-pattern . "8/DATE_UPDATED:[ 	]+\\\\?[\"<]+%:y-%02m-%02d %02H:%02M\\\\?[\">]")))
