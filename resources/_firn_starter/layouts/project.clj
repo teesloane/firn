be493975450cb1337599dda3b1272d5da816132e
teesloane <tylersloane@gmail.com>
2020-04-04 13:37:15 -0400

    Add: "Project" layout styling

--- resources/_firn_starter/layouts/project.clj
+++ resources/_firn_starter/layouts/project.clj
@@ -3,28 +3,31 @@
 
   (let [{:keys [head nav]} partials]
     (head
-     [:body.baskerville.flex.flex-column.lh-copy
+     [:body.baskerville.flex.flex-column.lh-copy.h-100
       (nav)
-      [:main.flex.flex-grow-1
-       [:article.br.overflow-scroll.pa4.w-50.vh-100
+      [:main.flex.flex-grow-1.h-100
+       [:article.br.flex-auto.overflow-scroll.pa4.w-50
         [:h1 title]
         [:div (render "Notes" :content)]]
-       [:aside.br.pa4.w-30.overflow-scroll.vh-100
-
+       [:aside.br.flex-auto.overflow-scroll.w-30
+        ;; good place for a partial.
         (when-let [resources (render "Resources" :content)]
           [:details {:open "true"}
-           [:summary  "Resources"]
-           [:div resources]])
+           [:summary.bb.pa3.bg-washed-blue   "Resources"]
+           [:div.bg-near-white.pa2.bb resources]])
 
         (when-not (empty? file-logs)
           [:details
-           [:summary  "Logbook"]
-           [:div (for [x file-logs
-                       :let [{:keys [year month day]} (x :start)]]
-                   [:div
-                    [:span year "/" month "/" day " - " (x :duration)]])]])
+           [:summary.bb.pa3.bg-washed-yellow  "Logbook"]
+           [:div.bg-near-white.pa3.bb
+            (for [x file-logs
+                  :let [{:keys [year month day]} (x :start)]]
+              [:div
+               [:span year "/" month "/" day " - " (x :duration)]])]])
 
         (when-let [tasks-headline (render "Tasks" :title-raw)]
           [:details
-           [:summary tasks-headline]
-           [:div (render "Tasks" :content)]])]]])))
+           [:summary.bb.pa3.bg-washed-red tasks-headline]
+           [:div.bg-near-white.pa3.bb (render "Tasks" :content)]])]
+       [:aside.br.flex-auto.overflow-scroll.pa4.w-20
+        "Right bar"]]])))
