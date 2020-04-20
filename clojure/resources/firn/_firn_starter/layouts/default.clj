fe90509393f63c0bd8b0e6568c62d2b589aafe1f
teesloane <tylersloane@gmail.com>
2020-04-20 19:57:44 -0400

    Clean: default css for default layout.

--- clojure/resources/firn/_firn_starter/layouts/default.clj
+++ clojure/resources/firn/_firn_starter/layouts/default.clj
@@ -1,8 +1,9 @@
 (defn default
   [{:keys [ yield partials]}]
-  (let [{:keys [head]} partials]
+  (let [{:keys [head]} partials
+        default-styles {:style "max-width: 700px; padding: 32px; margin: 0 auto"}]
     (head
-     [:body
+     [:body default-styles
       [:main
        [:article
         [:div yield]]
