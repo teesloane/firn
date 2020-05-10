bd19c6fd9df5ffd4a8e1fff221aef8eb8e593d68
teesloane <tylersloane@gmail.com>
2020-05-10 16:25:07 -0400

    Docs: remove comment.

--- clojure/resources/firn/_firn_starter/partials/head.clj
+++ clojure/resources/firn/_firn_starter/partials/head.clj
@@ -4,6 +4,5 @@
    [:head
     [:link {:rel "stylesheet" :href "/static/css/bass.css"}]
     [:meta {:charset "UTF-8"}]
-;; <meta http-equiv="Content-type" value="text/html; charset=UTF-8" />
     [:link {:rel "stylesheet" :href "/static/css/main.css"}]]
    body])
