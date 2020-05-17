aef7a08d153486255e8a9a6ca2794c3f9f18ba93
teesloane <tylersloane@gmail.com>
2020-05-16 22:44:24 -0400

    Fix: comment out serve invocation.

--- clojure/src/firn/server.clj
+++ clojure/src/firn/server.clj
@@ -155,6 +155,6 @@
 
 ;; (build/new-site {:dir-files "/Users/tees/Dropbox/wiki"})
 ;; (build/all-files {:dir-files "/Users/tees/Dropbox/wiki"})
-(serve {:dir-files "/Users/tees/Projects/firn/firn/clojure/test/firn/demo_org"})
+;; (serve {:dir-files "/Users/tees/Projects/firn/firn/clojure/test/firn/demo_org"})
 ;; (serve {:dir-files "/Users/tees/Dropbox/wiki"})
 ;; (mount/stop)
