e89f779ef85aa6347d929b70f4111046c4713e45
Tyler <teesloane@users.noreply.github.com>
2020-05-26 14:43:44 -0400

    Refactor: Replace cli-matic with tools.cli. (#12)
    * Internal: dir-files -> dir.
    * Internal: invoke cli-options at runtime to get cwd.
    * Clean: Remove server warning.

--- clojure/resources/firn/_firn_starter/partials/head.clj
+++ clojure/resources/firn/_firn_starter/partials/head.clj
@@ -2,7 +2,6 @@
   [body]
   [:html
    [:head
-    [:link {:rel "stylesheet" :href "/static/css/bass.css"}]
     [:meta {:charset "UTF-8"}]
     [:link {:rel "stylesheet" :href "/static/css/main.css"}]]
    body])
