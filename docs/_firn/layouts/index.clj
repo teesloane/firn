a5de56104c202f8726ea89491a73b659a72dbf26
teesloane <tylersloane@gmail.com>
2020-06-24 16:53:20 -0400

    Add: _firn site to docs.

---
+++ docs/_firn/layouts/index.clj
@@ -0,0 +1,21 @@
+(defn header
+  []
+  [:header.index-nav
+   [:div.flex.align-start
+    [:h1.h2.m0 "Firn"]
+    [:a {:target "_blank" :href "https://github.com/theiceshelf/firn/releases"}
+     [:sup.self-start "v.0.0.5"]]]
+   [:div.h4.my2.italics "A static site generator for org-mode."]
+   [:div.index-download-btns
+    [:a.mx2 {:target "_blank" :href "https://github.com/theiceshelf/firn/releases"} "Download latest"]
+    [:a.mx2 {:href "/getting-started"} "Read documentation"]]])
+
+(defn index
+  [{:keys [render partials] :as data}]
+  (let [{:keys [head]} partials]
+    (head
+     [:body
+      (header)
+      [:main.content
+       [:div.py3
+        (render "Details" :content)]]])))
---
+++ docs/_firn/partials/head.clj
@@ -0,0 +1,13 @@
+(defn head
+  [body]
+  [:html
+   [:head
+    [:meta {:charset "UTF-8"}]
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+    [:link {:rel "stylesheet" :href "/static/css/hljs-tomorrow.css"}]
+    [:link {:rel "stylesheet" :href "/static/css/firn_base.css"}]
+    [:link {:rel "stylesheet" :href "/static/css/basscss.css"}]
+    [:link {:rel "stylesheet" :href "/static/css/customization.css"}]
+    [:script {:src "/static/js/highlight.pack.js"}]
+    [:script "hljs.initHighlightingOnLoad()"]]
+   body])
