81b3c4632faba2d3e96a49787dea0e0c659a2ebf
Tyler <teesloane@users.noreply.github.com>
2020-07-05 18:08:12 -0400

    Feat: add tags to headlines + tag page. (#26)
    * Feat: add tags to headlines + tag page.
    
    * Test: fix tests and add tests for tags.
    
    * Add: Improve base css and tags.
    
    * Docs: add changelog.

--- clojure/resources/firn/_firn_starter/pages/tags.clj
+++ clojure/resources/firn/_firn_starter/pages/tags.clj
@@ -1,3 +1,17 @@
 (defn tags
-  []
-  [:h1 "Tags"])
+  [{:keys [site-tags partials]}]
+  (let [{:keys [head]} partials]
+    (head
+     [:body
+      [:main
+       [:article
+        [:div.content
+         [:h1 "Tags"]
+         (for [[tag-name tags] site-tags]
+           [:div
+            [:h2 {:id tag-name :class "firn-tag-heading"} tag-name]
+            (for [tag tags]
+              [:div
+               [:a {:href (tag :headline-link)}
+                (tag :from-file) " - "
+                (tag :from-headline)]])])]]]])))
