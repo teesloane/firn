90bcb06d239b6f776b66bee7d91326f236c8c542
teesloane <teesloane@users.noreply.github.com>
2020-07-11 13:57:11 +0000

    deploy: da54decc0aaf1a18b41f4104f5c312f924d9c60e

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  var x = document.getElementById("nav-icon")
+  var sb = document.getElementById("sidebar")
+  x.classList.toggle("open")
+  sb.classList.toggle("def-sidebar-open")
+
+}
