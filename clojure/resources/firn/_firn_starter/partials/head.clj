ef4e479bd8c43500a85e1468abdffa88a3fb0de0
teesloane <tylersloane@gmail.com>
2020-07-30 11:35:38 -0400

    Refactor: firn starter uses build-url + better styles.

--- clojure/resources/firn/_firn_starter/partials/head.clj
+++ clojure/resources/firn/_firn_starter/partials/head.clj
@@ -1,9 +1,7 @@
 (defn head
-  [site-url body]
-  (let [resource #(str site-url "/static/" %)]
-    [:html
-     [:head
-      [:meta {:charset "UTF-8"}]
-      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
-      [:link {:rel "stylesheet" :href (resource "css/firn_base.css")}]]
-     body]))
+  [build-url]
+  [:html
+   [:head
+    [:meta {:charset "UTF-8"}]
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+    [:link {:rel "stylesheet" :href (build-url "/static/css/firn_base.css")}]]])
