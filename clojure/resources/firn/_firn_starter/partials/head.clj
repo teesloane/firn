a2a6b4037a30daab27822a27371e3f7f3ad50dd2
Yosevu Kilonzo <yosevu@heysparkbox.com>
2020-10-01 08:23:40 -0400

    Fix: add site title, author, and description to layouts (#41)
    - Add site title, author, and description to layouts public-api for SEO and accessibility.
    - Update default layout
    - Update head partial
    - Make meta charset casing consistent with other attributes
    
    - Update metadata table
      - Add site-author, site-desc, and site-title
      - Add site-url
      - Add firn-tags data type
      - Alphabetize and format

--- clojure/resources/firn/_firn_starter/partials/head.clj
+++ clojure/resources/firn/_firn_starter/partials/head.clj
@@ -1,7 +1,10 @@
 (defn head
-  [build-url]
+  [{:keys [build-url site-title site-author site-desc]}]
   [:html
    [:head
-    [:meta {:charset "UTF-8"}]
+    [:meta {:charset "utf-8"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+    [:meta {:name "author" :content site-author}]
+    [:meta {:name "description" :content site-desc}]
+    [:title site-title]
     [:link {:rel "stylesheet" :href (build-url "/static/css/firn_base.css")}]]])
