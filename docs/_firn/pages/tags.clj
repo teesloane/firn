ca986191fde1b1135b45540f79b19e2558d08297
teesloane <tylersloane@gmail.com>
2020-10-20 18:14:53 -0400

    Docs: add related-files docs; merge with backlinks
    + some light css cleanup.

--- docs/_firn/pages/tags.clj
+++ docs/_firn/pages/tags.clj
@@ -9,10 +9,11 @@
        [:article.def-wrapper
         [:aside#sidebar.def-sidebar
          (render :sitemap {:sort-by :firn-order})]
-        [:div.def-content
+        [:div.def-content.mb2
          [:h1 "Org Tags"]
          (render :org-tags)
          [:hr]
          [:h1 "File Tags"]
-         (render :firn-tags)
+         [:div.pb4
+          (render :firn-tags)]
          (footer)]]]]]))
