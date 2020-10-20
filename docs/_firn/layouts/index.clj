5cdd88c09f5662dd6a7b1061be7e8bb4358ecbbe
teesloane <tylersloane@gmail.com>
2020-10-20 19:43:40 -0400

    Docs: changelog + dev tooling.

--- docs/_firn/layouts/index.clj
+++ docs/_firn/layouts/index.clj
@@ -4,7 +4,7 @@
    [:div.flex.align-start
     [:h1.h2.m0 "Firn"]
     [:a {:target "_blank" :href "https://github.com/theiceshelf/firn/releases"}
-     [:sup.self-start "v.0.0.9"]]]
+     [:sup.self-start "v.0.0.10"]]]
    [:div.h4.my2.italics "A static site generator for org-mode."]
    [:div.index-download-btns
     [:a.mx2 {:target "_blank" :href "https://github.com/theiceshelf/firn/releases"} "Download latest"]
