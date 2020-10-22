6f4a7d129f66f17b2d8a33e24ed2e4373cc5a00e
teesloane <tylersloane@gmail.com>
2020-10-22 07:17:44 -0400

    Docs: Migrate to prism.js/css.
    - fix: sidebar scroll unsticking.

--- docs/_firn/partials/head.clj
+++ docs/_firn/partials/head.clj
@@ -4,10 +4,9 @@
    [:meta {:charset "UTF-8"}]
    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
    [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,400;0,500;1,400&display=swap"}]
-   [:link {:rel "stylesheet" :href (build-url "/static/css/hljs-tomorrow.css")}]
+   [:link {:rel "stylesheet" :href (build-url "/static/css/prism.css")}]
    [:link {:rel "stylesheet" :href (build-url "/static/css/firn_base.css")}]
    [:link {:rel "stylesheet" :href (build-url "/static/css/basscss.css")}]
    [:link {:rel "stylesheet" :href (build-url "/static/css/customization.css")}]
    [:link {:rel "icon" :type "image/png" :href (build-url "/static/img/favico.png")}]
-   [:script {:src (build-url "/static/js/highlight.pack.js")}]
-   [:script "hljs.initHighlightingOnLoad()"]])
+   [:script {:src (build-url "/static/js/prism.js")}]])
