303f23d977b4fa284311a4fae8b49be7e1e639c1
teesloane <tylersloane@gmail.com>
2020-11-04 21:32:24 -0500

    Add:[repl] bind pprint and create help fns.

--- clojure/src/firn/repl.clj
+++ clojure/src/firn/repl.clj
@@ -1,16 +1,38 @@
 (ns firn.repl
-  (:require [sci.core :as sci]))
+  (:require [sci.core :as sci]
+            [clojure.pprint :as pprint]
+            [clojure.string :as s]))
 
 ;; -- REPL Functions (The Repl "API") --
 
 ;; TODO: reload!
+
+(defn help
+  []
+  (println
+   (->> ["The repl can help you learn about the data moving around a Firn site when it runs."
+         ""
+         "The following functions are available to you:"
+         ""
+         "# Bound Clojure functions --"
+         "(pprint m)    ; pretty print a clojure value"
+
+         ""
+         "# Available Data --"
+         "@config       ; de-reference and view the config atom (it's large!)"
+         ""
+         "# Custom Functions --"
+         "(help)        ; print this message"]
+        (s/join \newline)
+        )))
+
 
 ;; -- REPL setup.
 
 (defn prompt [ctx]
   ;; fetch the current namespace name for printig a prompt
   (let [ns-name (sci/eval-string* ctx "(ns-name *ns*)")]
-    (print (str ns-name "> "))
+    (print (str ns-name "=> "))
     (flush)))
 
 (defn handle-error [_ctx last-error e]
@@ -19,7 +41,9 @@
 
 (defn sci-bindings
   [config]
-  {'config config})
+  {'config  config
+   'help    help
+   'pprint  pprint/pprint})
 
 (defn init [config]
   (let [;; we are going to read Clojure expressions from stdin
