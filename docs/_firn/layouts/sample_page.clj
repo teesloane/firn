5ae9ebd88a875cfab5832abb77f8065e2b13843f
teesloane <tylersloane@gmail.com>
2020-12-13 16:10:55 -0500

    Docs: fix syntax highlighting on sample_page.

--- docs/_firn/layouts/sample_page.clj
+++ docs/_firn/layouts/sample_page.clj
@@ -18,32 +18,32 @@
          [:div "The following examples demonstrate the ability of the 'render' function; each code sample demonstrates usage, as well as the output below it."]
          [:div
           [:h3 "Rendering a specific heading"]
-          [:pre [:code "(render \"Find and render me!\")"]]
+          [:pre.language.language-clojure [:code "(render \"Find and render me!\")"]]
           (render "Find and render me!")
           [:hr]]
          [:div
           [:h3 "Rendering a table of contents"]
-          [:pre [:code "(render :toc)"]]
+          [:pre.language.language-clojure [:code "(render :toc)"]]
           (render :toc)
           [:hr]]
          [:div
           [:h3 "Rendering a table of contents, staring at a specific headline."]
-          [:pre [:code "(render :toc {:headline \"Headings\"})"]]
+          [:pre.language.language-clojure [:code "(render :toc {:headline \"Headings\"})"]]
           (render :toc {:headline "Headings"})
           [:hr]]
          [:div
           [:h3 "Rendering a table of contents, up to a specific depth."]
-          [:pre [:code "(render :toc {:depth 1})"]]
+          [:pre.language.language-clojure [:code "(render :toc {:depth 1})"]]
           (render :toc {:depth 1})
           [:hr]]
          [:div
           [:h3 "Rendering a table of contents, up to a specific depth, starting at a specific heading, but excluding the heading started at."]
-          [:pre [:code "(render :toc {:headline \"Headings\" :depth 1 :exclude-headline? true})"]]
+          [:pre.language.language-clojure [:code "(render :toc {:headline \"Headings\" :depth 1 :exclude-headline? true})"]]
           (render :toc {:headline "Headings" :depth 1 :exclude-headline? true})
           [:hr]]
          [:div
           [:h3 "Rendering the sum Logbook of this file, as a polyline svg. This is a rather opinionated 'render'; it sorts your logbooks into years and the renders a graph for each year. "]
-          [:pre [:code "(render :logbook-polyline {:height 70 :stroke \"salmon\"})"]]
+          [:pre.language.language-clojure [:code "(render :logbook-polyline {:height 70 :stroke \"salmon\"})"]]
           (render :logbook-polyline {:height 70 :stroke "salmon"})
           [:hr]]]]]
       (footer)]]))
