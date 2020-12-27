c7f5e0cbbeffbb1418ea58f38d4c8e20cbf9cab9
teesloane <tylersloane@gmail.com>
2020-12-27 11:19:20 -0500

    Clean: Rename borrowed code for JNI fns.

--- clojure/src-java/iceshelf/clojure/rust/ClojureRust.java
+++ clojure/src-java/iceshelf/clojure/rust/ClojureRust.java
@@ -1,14 +1,14 @@
 package iceshelf.clojure.rust;
 
 public class ClojureRust {
-    // This declares that the static `getFreeMemory` method will be provided
+    // This declares that the static `parseOrg` method will be provided
     // a native library.
-    private static native String getFreeMemoryRust(String unit);
+    private static native String parseOrgRust(String unit);
 
     // The rest is just regular ol' Java!
 
-    public static String getFreeMemory(String unit) throws java.io.IOException {
-        String output = getFreeMemoryRust(unit);
+    public static String parseOrg(String unit) throws java.io.IOException {
+        String output = parseOrgRust(unit);
         return output;
     }
 }
